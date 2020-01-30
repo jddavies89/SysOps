6d93c2a541a880d439e4a0b3283a9e3e3c5424f4
Joe Richards <richardswjoe@gmail.com>
2020-01-30 14:51:48 +0000

    Update SimplePwdResetToolv2.py

--- Python/SimplePwdResetToolv2.py
+++ Python/SimplePwdResetToolv2.py
@@ -1,23 +1,30 @@
-#Author Joe Richards
-#Date 12/12/2019.
-#Python version 3
-#Descriotion: Bored of resetting passwords for people? job done.
-#To do:
-#   1.  under section 'user.set_password("YourPassw0rdH3r3")', type a password of your choice.
-#   2.  Under section 'print(user, "\npassword has been reset to YourPassw0rdH3r3")', type a password of your choice.
-#Output of program in execution.
-#
-#Active Directory username: User1
-#Do you want to reset the password for AD User1 ?
-#y = yes, anykey = quit; y
-#<ADUser 'CN=User1,OU=Finance,OU=England,OU=Users,OU=Org,DC=Domain,DC=com'> 
-#password has been reset to YourPassw0rdH3r3
-#Active Directory username:
-#
-#
-#Download pypiwin32 from pip.
-#pip install pypiwin32
+'''
+Author          :   Joe Richards
+Date            :   12/12/2019.
+Python version  :   3
+Descriotion:    :   Bored of resetting passwords for people? job done.
+        
+                    To do:
+                       1.  under section 'user.set_password("YourPassw0rdH3r3")', type a password of your choice.
+                       2.  Under section 'print(user, "\npassword has been reset to YourPassw0rdH3r3")', type a password of your choice.
+                    
+Link            :   https://github.com/joer89/SysOps.git
+
+Output of program in execution.
+
+Active Directory username: User1
+Do you want to reset the password for AD User1 ?
+y = yes, anykey = quit; y
+<ADUser 'CN=User1,OU=Finance,OU=England,OU=Users,OU=Org,DC=Domain,DC=com'> 
+password has been reset to YourPassw0rdH3r3
+Active Directory username:
+
+
+Download pypiwin32 from pip.
+pip install pypiwin32
 from pyad import *
+
+'''
 
 def menu():    
     usr = input("Active Directory username: ")
