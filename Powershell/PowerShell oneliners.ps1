8b66d68ac2800ce266497f6084046140739b8782
Joe Richards <richardswjoe@gmail.com>
2020-01-31 14:08:38 +0000

    Update PowerShell oneliners.ps1

--- Powershell/PowerShell oneliners.ps1
+++ Powershell/PowerShell oneliners.ps1
@@ -73,11 +73,11 @@ Get-PSSession | Remove-PSSession
 Get-MessageTrace -RecipientAddress itsupport@theregisschool.co.uk -StartDate 01/30/2020 -EndDate 01/31/2020
 
 #Message trace on all out going emails on 31 of Jan 20 
-Get-MessageTrace -SenderAddress itsupport@theregisschool.co.uk -StartDate 01/30/2020 -EndDate 01/31/2020
+Get-MessageTrace -SenderAddress <Sender_address> -StartDate 01/30/2020 -EndDate 01/31/2020
 
 #Get a more detailed message trace with the following headings; Receive, submit, Transport rule, deliver and spam diagnostics.
-Get-MessageTrace -SenderAddress itsupport@theregisschool.co.uk -StartDate 01/30/2020 -EndDate 01/31/2020 | Get-MessageTraceDetail
+Get-MessageTrace -SenderAddress <Sender_address> -StartDate 01/30/2020 -EndDate 01/31/2020 | Get-MessageTraceDetail
 
 #Get a more detailed message trace with sender and receiver emails, you can pipe Format-List for a more detailed list again.
-Get-MessageTrace -SenderAddress John.Crittenden@theregisschool.co.uk -RecipientAddress itsupport@theregisschool.co.uk -StartDate 01/30/2020 -EndDate 01/31/2020
+Get-MessageTrace -SenderAddress <Sender_address> -RecipientAddress <Receiver_Address> -StartDate 01/30/2020 -EndDate 01/31/2020
 
